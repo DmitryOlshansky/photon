d03ad582d03083dbaf4a53eb0ad08a18206df517
Dmitry Olshansky <dmitry@olshansky.me>
2025-11-06 17:57:23 +0300

    Fix pool example, pool alias this is mutable which writeln won't accept

--- examples/pool.d
+++ examples/pool.d
@@ -26,26 +26,26 @@ void main() {
     auto counts = pool(2, 1.seconds, () => atomicFetchAdd(active, 1), (ref int i) { atomicFetchAdd(closed, 1); });
     go({
         auto first = counts.acquire();
-        writeln("acquire: ", first);
+        writeln("acquire: ", first.get);
         auto second = counts.acquire();
-        writeln("acquire: ", second);
+        writeln("acquire: ", second.get);
         go({
             delay(1.seconds);
             counts.release(first);
             writeln("closed:", closed);
         });
         auto third = counts.acquire();
-        writeln("acquire:", third);
+        writeln("acquire:", third.get);
         counts.dispose(third); // now we have allocated == 1 and empty free list
         writeln("closed: ", closed);
         auto fourth = counts.acquire(); // so this should allocate new connection
-        writeln("acquire: ", fourth);
+        writeln("acquire: ", fourth.get);
         counts.release(fourth);
         counts.release(second);
         delay(2.seconds); // give time for the cleaner to cleanup stale connections
         writeln("closed: ", 3);
         auto fifth = counts.acquire();
-        writeln("acquire: ", fifth);
+        writeln("acquire: ", fifth.get);
         counts.shutdown();
         writeln("after shutdown");
     });
